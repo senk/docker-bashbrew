8d32d0d6dceab7d7418a78a303d21a3f128223ae
Robin Naundorf <r.naundorf@fh-muenster.de>
2016-06-13 16:16:22 +0200

    test

--- lint.sh
+++ lint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 FILES=`find . -name "Dockerfile"`
+EXIT_STATUS=0
 for file in $FILES
 do
-  docker run --rm --privileged -v `pwd`:/root/ projectatomic/dockerfile-lint dockerfile_lint -r basic_rules.yaml -f "$file" ';'
+  docker run --rm --privileged -v `pwd`:/root/ projectatomic/dockerfile-lint dockerfile_lint -r basic_rules.yaml -f "$file" ';' || EXIT_STATUS=$?
 done
+exit $EXIT_STATUS
