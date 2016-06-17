2a114d0b2ff5f5cdc07ab683992ad663fadd8d3e
Robin Naundorf <r.naundorf@fh-muenster.de>
2016-06-17 19:42:29 +0200

    dsfds

--- tools/lint.sh
+++ tools/lint.sh
@@ -3,6 +3,7 @@ FILES=`find . -name "Dockerfile"`
 EXIT_STATUS=0
 for file in $FILES
 do
+  echo "Linting $file"
   docker run --rm --privileged -v `pwd`:/root/ projectatomic/dockerfile-lint dockerfile_lint -r basic_rules.yaml -f "$file" ';' || EXIT_STATUS=$?
 done
 exit $EXIT_STATUS
--- tools/securityscan.sh
+++ tools/securityscan.sh
@@ -2,5 +2,6 @@
 IMAGES=`bashbrew list --all`
 for image in $IMAGES
 do
+  echo "Scanning $image"
   sudo oscap-docker image-cve $image --result "$image.xml" --report "$image.html"
 done
