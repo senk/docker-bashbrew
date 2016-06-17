63e682587c85dbd086f8191ff501a3abb864aefa
Robin Naundorf <r.naundorf@fh-muenster.de>
2016-06-17 16:02:27 +0200

    te

--- funktionstest.sh
+++ funktionstest.sh
@@ -2,5 +2,6 @@
 IMAGES=`bashbrew list --all`
 for image in $IMAGES
 do
+  echo "Testing $image"
   docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`:/work -e TEST_IMAGE=$image serverspec
 done
