3c6abce6122f0026a89df770301c455f7b65d31b
Robin Naundorf <r.naundorf@fh-muenster.de>
2016-06-19 19:31:15 +0200

    dfssd

--- tools/funktionstest.sh
+++ tools/funktionstest.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 IMAGES=`bashbrew list --all`
+EXIT_STATUS=0
 for image in $IMAGES
 do
   echo "Testing $image"
-  docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`:/work -e TEST_IMAGE=$image serverspec
+  docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`:/work -e TEST_IMAGE=$image serverspec || EXIT_STATUS=$?
 done
+exit $EXIT_STATUS
